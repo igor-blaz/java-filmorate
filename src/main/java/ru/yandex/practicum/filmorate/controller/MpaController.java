79acff38a7a73cc62462f76116cae79a4f6addb1
Igor_blaz <koloda5600@gmail.com>
2025-04-12 15:22:41 +0300

    Исправлен codeStyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.List;
 
-@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/mpa")
@@ -17,13 +15,11 @@ public class MpaController {
 
     @GetMapping("/{id}")
     public Mpa getMpa(@PathVariable int id) {
-        log.info("Запрос на получение Mpa");
         return mpaService.getMpa(id);
     }
 
     @PostMapping
     public Mpa createMpa(@RequestBody Mpa mpa) {
-        log.info("Добавление нового рейтинга");
         return mpaService.addMpa(mpa);
     }
 
