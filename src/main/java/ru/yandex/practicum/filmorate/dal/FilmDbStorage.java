5927614ed14d765c565c495612a8917a8a896315
ea7hie <ea7hie@mail.ru>
2025-04-26 10:08:41 +0300

    Удален unused import - Map in FilmDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmDbStorage.java
@@ -12,7 +12,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
