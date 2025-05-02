7cff683b482592ced0734bf316fdb86f50f7301b
ea7hie <ea7hie@mail.ru>
2025-05-02 15:50:16 +0300

    удалены unused imports из GenreDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/dal/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/GenreDbStorage.java
@@ -4,7 +4,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.mapper.GenreRowMapper;
-import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.HashSet;
